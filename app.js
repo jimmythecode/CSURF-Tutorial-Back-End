a67f27cbaf3aab5d5c187d6b9fad5ed56b905ddd
jimmythecode <jimmythecode@gmail.com>
2021-01-20 17:45:10 +0000

    put cors back in

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var cookieParser = require('cookie-parser')
 var csrf = require('csurf')
 var bodyParser = require('body-parser')
 var express = require('express')
+var cors = require('cors')
 
 // setup route middlewares
 var csrfProtection = csrf({ cookie: true })
@@ -9,6 +10,13 @@ var parseForm = bodyParser.urlencoded({ extended: false })
 
 // create express app
 var app = express()
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+}
+
+app.use(cors(corsOptions));
 
 // parse cookies
 // we need this because "cookie" is true in csrfProtection
